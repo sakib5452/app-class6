03416a705597cf1a343ee3eeb354f9134b1d2cb6
najmul sakib <najmus191@gmail.com>
2023-11-22 10:02:41 +0600

    userinpur type

---
+++ src/main/kotlin/Userinput.kt
@@ -0,0 +1,9 @@
+fun main(){
+//    var name:String = readLine()!!
+//    println("My name is ${name}")
+
+    var num1:Int = readLine()!!.toInt()
+    var num2:Int = readLine()!!.toInt()
+    var sum = num1+num2
+    println("The sum is ${sum}")
+}
