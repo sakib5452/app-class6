eb23e0d25f8ddffae036a4227814142b9984cd1a
najmul sakib <najmus191@gmail.com>
2023-11-22 10:17:01 +0600

    java Scanner System

---
+++ src/main/kotlin/UserinputJava.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(){
+    var reader = Scanner(System.`in`)
+
+    var num1:Int = reader.nextInt()
+    var num2:Int = reader.nextInt()
+    var sum = num1+num2
+    println("The Sum is ${sum}")
+}
