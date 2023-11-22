117d6dc8a10c63fcab0f03e9f56b4b75e532e78e
najmul sakib <najmus191@gmail.com>
2023-11-22 09:51:50 +0600

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+   var a:Int = 10
+    println(a)
+
+//    Int to Double
+
+    var b:Double = a.toDouble()
+    println(b)
+    println(a.toDouble())
+
+//    String to Int
+    var str:String = "10"
+    println(str.toInt())
+
+// var name:String = "Musfiq"
+// println(name.toInt())
+//NB: aivabe kora jabe na
+
+ var ch:Char = 'A'
+ println(ch.toInt())
+
+var c:Int = 54802
+println(c.toByte())
+}
